eed0b230d23c91fe3db0dd82e7a0ca85676703b5
efzet <fauzi.padlaw@gmail.com>
2018-04-28 20:19:11 +0700

    added Procfile & edited TrustProxy middleware

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array
      */
-    protected $proxies;
+    protected $proxies = '*';
 
     /**
      * The current proxy header mappings.
@@ -25,5 +25,6 @@ class TrustProxies extends Middleware
         Request::HEADER_X_FORWARDED_HOST => 'X_FORWARDED_HOST',
         Request::HEADER_X_FORWARDED_PORT => 'X_FORWARDED_PORT',
         Request::HEADER_X_FORWARDED_PROTO => 'X_FORWARDED_PROTO',
+        Request::HEADER_X_FORWARDED_AWS_ELB,
     ];
 }
